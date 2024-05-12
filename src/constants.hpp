dfd9e7147d9d017d6e9af6b08134c5100827deb9
Maxime Joly <maxime.joly2808@gmail.com>
2024-05-12 21:55:16 +0200

    Update constants.hpp

--- src/constants.hpp
+++ src/constants.hpp
@@ -1,6 +1,7 @@
 #ifndef CONSTANTS_HPP
 #define CONSTANTS_HPP
 
-#define DLL_EXPORT false
+// Define DLL_EXPORT as true if compiling for Windows (_WIN32 is defined)
+#define DLL_EXPORT defined(_WIN32)
 
 #endif // CONSTANTS_HPP
